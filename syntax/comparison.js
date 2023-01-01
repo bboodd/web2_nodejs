7798420dba708f76a5070400d7e32853340d5801
bboodd <gkrgus9@gmail.com>
2023-01-01 12:46:47 +0900

    node3

---
+++ syntax/comparison.js
@@ -0,0 +1,7 @@
+console.log(1==1); // true 같으냐?
+console.log(1==2); // false
+console.log(1>2); // false
+console.log(1<2); // true
+console.log(1===1); //true 정확히 같으냐? (이걸써라)
+console.log(1===2); // false
+name = 1;
---
+++ syntax/conditional.js
@@ -0,0 +1,8 @@
+var args = process.argv;
+console.log(args[2]);
+
+if(args[2] === '1'){
+  console.log('A');
+} else {
+  console.log('B');
+}
