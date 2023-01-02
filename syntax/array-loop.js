7ea0e4d71615dfcad9f2c7513a6eb168133bbe40
bboodd <gkrgus9@gmail.com>
2023-01-02 09:37:45 +0900

    node3

---
+++ syntax/array-loop.js
@@ -0,0 +1,8 @@
+var numbers = [1,400,12,34,5];
+var i = 0;
+var total = 0;
+while(i < numbers.length){
+  total+=numbers[i];
+  i+=1;
+}
+console.log(`total : ${total}`);
---
+++ syntax/function2.js
@@ -0,0 +1,8 @@
+console.log(Math.round(1.6));
+console.log(Math.round(1.4));
+
+function sum(first, second){
+  return first+second; // return은 값 출력과 함수 종료 2가지의미
+}
+
+sum(2,4);
